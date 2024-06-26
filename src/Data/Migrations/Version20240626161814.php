aafac6114c0707d7147d0a870de2fbd24bf1c9c2
ChrisDBrown <owner@chrisdbrown.co.uk>
2024-06-26 17:24:22 +0100

    fix: Standards

--- src/Data/Migrations/Version20240626161814.php
+++ src/Data/Migrations/Version20240626161814.php
@@ -9,11 +9,13 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20240626161814 extends AbstractMigration
 {
+    #[\Override]
     public function getDescription(): string
     {
         return 'No idea what this is complaining about tbh, maybe DateTimeImmutable?';
     }
 
+    #[\Override]
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, username, roles, last_read_offset, created_at FROM user');
@@ -27,6 +29,7 @@ final class Version20240626161814 extends AbstractMigration
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON user (username)');
     }
 
+    #[\Override]
     public function down(Schema $schema): void
     {
         // No down migration should be added, follow expand-contract practices
